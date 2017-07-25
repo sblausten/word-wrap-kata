83617dd44061ae6c0ef649fa1086d1c98fb6b531
Sam Blausten <sblausten@gmail.com>
2017-07-25 19:58:53 +0100

    Unrefactored but now inserts linebreak every nth columnlength

--- src/Wrapper.java
+++ src/Wrapper.java
@@ -1,8 +1,34 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Wrapper {
 
 	public static String wrap(String text, int columnLength) {
-		return text;
+		int textLength = text.length();
+		char[] charsArray = text.toCharArray();
+		List<String> textSections = new ArrayList<String>();
+		int sectionCount = textLength / columnLength;
+		
+		int charNum = 0;
+		for (int i = 0; i < sectionCount; i++) {
+			String section = "";
+			for (int n = 0; n < columnLength; n++) {
+				section += charsArray[charNum];
+				charNum++;
+			}
+			textSections.add(section);
+		}
+		int remainingChars = textLength % columnLength;
+		if(remainingChars > 0) {
+			String section = "";
+			for (int n = 0; n < remainingChars; n++) {
+				section += charsArray[charNum];
+				charNum++;
+			}
+			textSections.add(section);
+		}
+		return text.join("\n", textSections);
 	}
 
 }
--- test/WrapperTest.java
+++ test/WrapperTest.java
@@ -1,13 +1,28 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class WrapperTest {
 
 	@Test
 	public void wrap_ShouldReturnString() {
-		String testString = "blaa blaa blaa blaa blaaaa";
-		assertEquals(testString, Wrapper.wrap(testString, 10));
+		assertEquals("", Wrapper.wrap("", 5));
 	}
+	
+	@Test
+	public void wrap_ShouldReturnStringWithBreakAfterEvery5Chars() {
+		String testString = "blaa blaa blaa rrrr blaaaa";
+		String expectedString = "blaa \nblaa \nblaa \nrrrr \nblaaa\na";
+		assertEquals(expectedString, Wrapper.wrap(testString, 5));
+	}
+	
+//	@Test
+//	public void splitString_ShouldReturnArrayOfChars() {
+//		String testString = "blaa blaa";
+//		char[] expectedArray = {'b', 'l', 'a', 'a', ' ', 'b', 'l', 'a', 'a'};
+//		assertEquals(expectedArray, Wrapper.toChars(testString));
+//	}
 
 }
